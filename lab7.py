e5b65e9411b72f61670104b535a2dbe076804e0b
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2024-11-26 20:08:16 +0100

    node class added

---
+++ lab7.py
@@ -0,0 +1,11 @@
+
+
+
+class Node:
+    def __init__(self, x,y,is_bc=False):
+        self.x = x
+        self.y = y
+        self.is_bc = is_bc          # flaga okreslajaca czy wezel jest brzegowy
+
+
+
