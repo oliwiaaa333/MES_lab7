4343fe4d6647ceb4600e1be4c3e77a03ec96d35b
oliwiaaa333 <wiatrowskaoliwiaxxx@gmail.com>
2024-11-26 20:23:15 +0100

    element class added

--- lab7.py
+++ lab7.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+import math
 
 
 class Node:
@@ -8,4 +9,69 @@ class Node:
         self.is_bc = is_bc          # flaga okreslajaca czy wezel jest brzegowy
 
 
+class Element:
+    def __init__(self, nodes, element_type):
+        self.nodes = nodes
+        self.H_local = None
+        self.H_bc = None
+        self.element_type = element_type
 
+    def shape_functions(self, xi, eta):
+        if self.element_type == "4-node":
+            return np.array([
+                (1 - xi) * (1 - eta) / 4,
+                (1 + xi) * (1 - eta) / 4,
+                (1 + xi) * (1 + eta) / 4,
+                (1 - xi) * (1 + eta) / 4,
+                ])
+        elif self.element_type == "9-node":
+            return np.array([
+                1 / 4 * (1 - xi) * (1 - eta) * xi * eta,
+                -1 / 4 * (1 + xi) * (1 - eta) * xi * eta,
+                1 / 4 * (1 + xi) * (1 + eta) * xi * eta,
+                -1 / 4 * (1 - xi) * (1 + eta) * xi * eta,
+                -1 / 2 * (1 - xi**2) * (1 - eta) * eta,
+                1 / 2 * (1 + xi) * (1 - eta**2) * xi,
+                -1 / 2 * (1 - xi**2) * (1 + eta) * eta,
+                -1 / 2 * (1 - xi) * (1 - eta**2) * xi,
+                (1 - xi**2) * (1 - eta**2),
+                ])
+
+    def shape_function_derivatives(self, xi, eta):
+        if self.element_type == "4-node":
+            dN_dxi = np.array([
+                -(1 - eta) / 4,
+                (1 - eta) / 4,
+                (1 + eta) / 4,
+                -(1 + eta) / 4,
+                ])
+            dN_deta = np.array([
+                -(1 - xi) / 4,
+                -(1 + xi) / 4,
+                (1 + xi) / 4,
+                (1 - xi) / 4,
+                ])
+        elif self.element_type == "9-node":
+            dN_dxi = np.array([
+                1 / 4 * (1 - eta) * (2 * xi * eta - eta),
+                -1 / 4 * (1 - eta) * (2 * xi * eta + eta),
+                1 / 4 * (1 + eta) * (2 * xi * eta + eta),
+                -1 / 4 * (1 + eta) * (2 * xi * eta - eta),
+                -1 / 2 * 2 * xi * (1 - eta) * eta,
+                1 / 2 * (1 - eta**2) * (2 * xi + 1),
+                -1 / 2 * 2 * xi * (1 + eta) * eta,
+                -1 / 2 * (1 - eta**2) * (2 * xi - 1),
+                -2 * xi * (1 - eta**2),
+                ])
+            dN_deta = np.array([
+                1 / 4 * (1 - xi) * (xi * 2 * eta - xi),
+                -1 / 4 * (1 + xi) * (xi * 2 * eta + xi),
+                1 / 4 * (1 + xi) * (xi * 2 * eta + xi),
+                -1 / 4 * (1 - xi) * (xi * 2 * eta - xi),
+                -1 / 2 * (1 - xi**2) * (2 * eta - 1),
+                1 / 2 * (1 + xi) * (-2 * eta),
+                -1 / 2 * (1 - xi**2) * (2 * eta + 1),
+                -1 / 2 * (1 - xi) * (-2 * eta),
+                -2 * eta * (1 - xi**2),
+                ])
+        return dN_dxi, dN_deta
